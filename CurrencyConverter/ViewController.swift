302150cdf86bbf0bc5ef159c540c004822a82539
Ozan Çiçek <77584235+ozancck@users.noreply.github.com>
2022-10-29 20:41:25 +0300

    Response

--- CurrencyConverter/ViewController.swift
+++ CurrencyConverter/ViewController.swift
@@ -22,31 +22,49 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         
-        let url = URL(string: "https://raw.githubusercontent.com/atilsamancioglu/CurrencyData/main/currency.json")
-        
-        let session = URLSession.shared
-        
-        let task = session.dataTask(with: url!) { data, response, error in
-            if error != nil {
-                let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: UIAlertController.Style.alert)
-                let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default)
-                alert.addAction(okButton)
-                self.present(alert, animated: true)
-            }else {
-                
-                
-                
-                
-                
-            }
-            
-            
-            
+    
         }
-    }
-
+    
     @IBAction func getRatesClicked(_ sender: Any) {
+        let url = URL(string: "https://raw.githubusercontent.com/atilsamancioglu/CurrencyData/main/currency.json")
+               
+               let session = URLSession.shared
+               
+               //Closure
+               
+               let task = session.dataTask(with: url!) { (data, response, error) in
+                   if error != nil {
+                       let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: UIAlertController.Style.alert)
+                       let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+                       alert.addAction(okButton)
+                       self.present(alert, animated: true, completion: nil)
+                       
+                   } else {
+                       // 2.
+                       if data != nil {
+                           do {
+                           let jsonResponse = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as! Dictionary<String, Any>
+                               
+                               DispatchQueue.main.async {
+                                   print(jsonResponse)
+                               }
+                               
+                    } catch {
+                        print("error")
+                    }
+                }
+
+            }
+
+        }
+
+        task.resume()
     }
     
+    
+    
 }
+
+
+    
 

53ea6679f8f85b55e41e6caf837717a41b372332
Ozan Çiçek <77584235+ozancck@users.noreply.github.com>
2022-10-29 20:06:34 +0300

    Session(Request)

--- CurrencyConverter/ViewController.swift
+++ CurrencyConverter/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var cadLabel: UILabel!
+    @IBOutlet weak var chfLabel: UILabel!
+    @IBOutlet weak var gbpLabel: UILabel!
+    @IBOutlet weak var jpyLabel: UILabel!
+    @IBOutlet weak var usdLabel: UILabel!
+    @IBOutlet weak var tryLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        let url = URL(string: "https://raw.githubusercontent.com/atilsamancioglu/CurrencyData/main/currency.json")
+        
+        let session = URLSession.shared
+        
+        let task = session.dataTask(with: url!) { data, response, error in
+            if error != nil {
+                let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: UIAlertController.Style.alert)
+                let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default)
+                alert.addAction(okButton)
+                self.present(alert, animated: true)
+            }else {
+                
+                
+                
+                
+                
+            }
+            
+            
+            
+        }
     }
 
-
+    @IBAction func getRatesClicked(_ sender: Any) {
+    }
+    
 }
 
